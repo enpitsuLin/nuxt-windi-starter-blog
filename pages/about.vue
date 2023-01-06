6603b03ca03407affaff8a59d67f07affcb7361d
enpitsulin <enpitsulin@gmail.com>
2023-01-07 00:48:41 +0800

    feat: add about page

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const app = useAppConfig();
+</script>
+<template>
+  <SEO :title="`About - ${app.author}`" :description="app.description" />
+  <section>
+    <h2>/about</h2>
+  </section>
+</template>
