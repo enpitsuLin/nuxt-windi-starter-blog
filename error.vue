e5bdfad2c1b66992486b0a4feaf274614f6cef8b
enpitsulin <enpitsulin@gmail.com>
2023-01-06 10:50:41 +0800

    feat: error page

--- error.vue
+++ error.vue
@@ -1,1 +1,13 @@
-<template>error</template>
+<script setup lang="ts">
+  const props = defineProps<{ error: Object }>();
+  const backToHome = () => clearError({ redirect: '/' });
+</script>
+<template>
+  <section>
+    <h2>error page</h2>
+    <code>
+      <pre>{{ JSON.stringify(props, null, 2) }}</pre>
+    </code>
+    <button @click="backToHome">back to home</button>
+  </section>
+</template>
