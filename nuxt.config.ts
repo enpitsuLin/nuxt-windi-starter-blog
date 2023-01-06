423d7ac499d40e359509f673dbf127ed93f2c4f6
enpitsulin <enpitsulin@gmail.com>
2023-01-06 13:58:09 +0800

    feat: add unplugin-icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-windicss']
+  modules: ['nuxt-windicss', ['unplugin-icons/nuxt', { autoInstall: true }]]
 });
