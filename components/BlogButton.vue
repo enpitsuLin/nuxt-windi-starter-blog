0523c1e4416e1eedc577d9436ae7c5185ae2f1d0
enpitsulin <enpitsulin@gmail.com>
2023-01-08 15:41:15 +0800

    fix: BlogButton disabled style

--- components/BlogButton.vue
+++ components/BlogButton.vue
@@ -10,7 +10,7 @@
     :class="[
       'focus:outline-none focus:ring-offset-white focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 dark:focus:ring-offset-black dark:focus:ring-gray-50',
       'hover:(bg-gray-50 dark:bg-gray-900)',
-      disabled && 'cursor-not-allowed opacity-75'
+      disabled && '!cursor-not-allowed opacity-75'
     ]"
     class="bg-white border rounded-lg font-medium border-gray-200 text-base py-3 px-6 text-gray-700 inline-flex items-center justify-center sm:justify-start dark:bg-[#0c0c0d] dark:border-gray-700 dark:text-gray-200"
     :to="disabled ? undefined : to"
