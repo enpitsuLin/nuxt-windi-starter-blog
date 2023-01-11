d47cf9253d3f1615360dd96d8c1340bfdb927b63
enpitsulin <enpitsulin@gmail.com>
2023-01-11 17:01:26 +0800

    feat: add toc for [slug] page

---
+++ components/ProseToc.vue
@@ -0,0 +1,70 @@
+<script lang="tsx" setup>
+  import { TocLink } from '@nuxt/content/dist/runtime/types';
+  import { useToggle } from '@vueuse/core';
+  import { PropType } from 'vue';
+
+  defineProps<{ links: TocLink[] }>();
+
+  const [show, toggleShow] = useToggle(false);
+
+  const HeadingList = defineComponent({
+    props: { links: { type: Object as PropType<TocLink[]> } },
+    render() {
+      return (
+        <ul class="px-1">
+          {this.links?.map((link) => (
+            <li class={['min-w-0 group', link.depth > 1 && 'pl-3']}>
+              <a
+                href={`#${link.id}`}
+                class="text-sm py-1 text-gray-500 block truncate lg:pr-3 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-50"
+                dynamic-class={[
+                  false && 'font-semibold text-sm py-1 text-green-500 block truncate lg:pr-3 dark:text-green-400'
+                ]}
+              >
+                {link.text}
+              </a>
+              {link.children?.length !== 0 && <HeadingList links={link.children} />}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  });
+</script>
+
+<template>
+  <div
+    class="order-1 -mx-4 px-4 top-20 z-30 backdrop-filter overflow-x-hidden sticky backdrop-blur-md sm:-mx-6 sm:px-6 lg:mx-0 lg:-mt-8 lg:max-h-[calc(100vh-80px)] lg:px-0 lg:pt-8 lg:z-0 lg:col-span-2 lg:self-start"
+  >
+    <div class="border-b border-dashed border-gray-200 py-3 lg:border-none lg:py-0 dark:border-gray-700">
+      <button class="flex gap-3 items-center lg:hidden" tabindex="-1" @click="toggleShow()">
+        <span class="font-semibold text-sm text-gray-900 leading-6 dark:text-gray-50">Table of Contents</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          class="'h-5 transform transition-transform text-gray-400 w-5 duration-100 icon dark:text-gray-500'"
+          :class="[show ? 'rotate-90' : 'rotate-0']"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M15.54 11.29L9.88 5.64a1 1 0 0 0-1.42 0a1 1 0 0 0 0 1.41l4.95 5L8.46 17a1 1 0 0 0 0 1.41a1 1 0 0 0 .71.3a1 1 0 0 0 .71-.3l5.66-5.65a1 1 0 0 0 0-1.47Z"
+          ></path>
+        </svg>
+      </button>
+      <nav
+        class="flex flex-col space-y-1 mt-4 py-1 px-1 hidden sm:space-y-2 lg:mt-0 lg:block"
+        :style="show ? 'display:flex' : ''"
+      >
+        <span class="font-semibold text-sm items-center hidden overflow-hidden lg:flex">
+          <h2>Table of Contents</h2>
+        </span>
+        <HeadingList :links="links" />
+      </nav>
+    </div>
+  </div>
+</template>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -15,55 +15,70 @@
 <template>
   <template v-if="data">
     <SEO :title="data.title ?? ''" :description="data.description" />
-    <article>
-      <div class="xl-divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-        <header class="pt-12 xl:pb-4">
-          <div class="border-b space-y-2 border-gray-200 text-center pb-12 dark:border-gray-700">
-            <h1
-              class="font-extrabold tracking-tight text-3xl text-gray-900 leading-9 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 dark:text-gray-100"
-            >
-              {{ data.title }}
-            </h1>
-            <dl>
-              <div>
-                <dt class="sr-only">Published on</dt>
-                <dd class="font-medium text-base text-gray-500 leading-6 dark:text-gray-400">
-                  <time :datetime="data.date">{{ format(data.date) }}</time>
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </header>
-        <div class="divide-y divide-gray-200 pb-8 xl:divide-y-0 dark:divide-gray-700">
-          <div class="divide-y divide-gray-200 xl:pb-0 dark:divide-gray-700">
-            <div class="max-w-none pt-4 pb-8">
-              <ContentRenderer :value="data" />
-            </div>
-            <div class="py-6 text-gray-700 dark:text-gray-200">Comments</div>
-          </div>
-          <footer>
-            <div
-              class="font-medium divide-gray-200 text-sm leading-5 xl:divide-y xl:col-start-1 xl:row-start-2 dark:divide-gray-700"
-            >
-              <div v-if="next || prev" class="flex py-4 justify-between xl:space-y-8 xl:py-8 xl:block">
-                <div v-if="prev">
-                  <h2 class="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">Previous Article</h2>
-                  <div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                    <NuxtLink :href="`/blog/${prev.slug}`">{{ prev.title }}</NuxtLink>
-                  </div>
-                </div>
+    <div class="col-span-10 relative lg:col-span-8">
+      <section class="flex flex-col-reverse gap-8 relative lg:grid lg:grid-cols-10">
+        <ProseToc v-if="data.body.toc.links.length > 0" :links="data.body.toc.links" />
 
-                <div v-if="next">
-                  <h2 class="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">Next Article</h2>
-                  <div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                    <NuxtLink :href="`/blog/${next.slug}`">{{ next.title }}</NuxtLink>
+        <article class="col-span-10 docs-page lg:col-span-8">
+          <div class="xl-divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+            <header class="pt-6 xl:pb-4 border-b border-gray-200 dark:border-gray-700">
+              <div class="space-y-2 text-center">
+                <dl>
+                  <div>
+                    <dt class="sr-only">Published on</dt>
+                    <dd class="font-medium text-base text-gray-500 leading-6 dark:text-gray-400">
+                      <time :datetime="data.date">{{ format(data.date) }}</time>
+                    </dd>
                   </div>
+                </dl>
+                <h1
+                  class="font-extrabold tracking-tight text-3xl text-gray-900 leading-9 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 dark:text-gray-100"
+                >
+                  {{ data.title }}
+                </h1>
+                <div v-if="data.image" class="pt-4">
+                  <NuxtImg
+                    class="rounded-lg h-full w-full"
+                    :src="data.image"
+                    fit="cover"
+                    :alt="data.title"
+                    loading="lazy"
+                  />
                 </div>
               </div>
+            </header>
+            <div class="divide-y divide-gray-200 pb-8 xl:divide-y-0 dark:divide-gray-700">
+              <div class="divide-y divide-gray-200 xl:pb-0 dark:divide-gray-700">
+                <div class="max-w-none pb-8">
+                  <ContentRenderer :value="data" />
+                </div>
+                <div class="py-6 text-gray-700 dark:text-gray-200">Comments</div>
+              </div>
+              <footer>
+                <div
+                  class="font-medium divide-gray-200 text-sm leading-5 xl:divide-y xl:col-start-1 xl:row-start-2 dark:divide-gray-700"
+                >
+                  <div v-if="next || prev" class="flex py-4 justify-between xl:space-y-8 xl:py-8 xl:block">
+                    <div v-if="prev">
+                      <h2 class="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">Previous Article</h2>
+                      <div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                        <NuxtLink :href="`/blog/${prev.slug}`">{{ prev.title }}</NuxtLink>
+                      </div>
+                    </div>
+
+                    <div v-if="next">
+                      <h2 class="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">Next Article</h2>
+                      <div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                        <NuxtLink :href="`/blog/${next.slug}`">{{ next.title }}</NuxtLink>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </footer>
             </div>
-          </footer>
-        </div>
-      </div>
-    </article>
+          </div>
+        </article>
+      </section>
+    </div>
   </template>
 </template>
